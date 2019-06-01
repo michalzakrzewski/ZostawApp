78dd4e3a3529930e1780a17ee21cdfdbe571ab5f
Michał Zakrzewski <michal.zakrzewsky@gmail.com>
2019-06-01 13:17:36 +0200

    init commit

---
+++ src/main/java/com/zakrzewski/zostawapp/ZostawApp/ZostawAppApplication.java
@@ -0,0 +1,13 @@
+package com.zakrzewski.zostawapp.ZostawApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZostawAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZostawAppApplication.class, args);
+	}
+
+}
